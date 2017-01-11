9627e3688b3f7a3f8a753aa204b68d176c9c5e2c
Margaret Green <mgreen@hfcc.edu>
2017-01-10 19:57:11 -0500

    Added ParentClass.java

---
+++ ParentClass.java
@@ -0,0 +1,33 @@
+/**
+ * Parent Class for adding to with GitHub
+ * @author greenml
+ *
+ */
+public class ParentClass {
+	private String name; // holds name of parent object
+	private int age;	 // holds age of parent object
+   private double income; // adding income field for parent
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+   public void setIncome(double income){
+      this.income = income;
+   }
+   
+   public double getIncome()  {
+      return income;
+   }
+	
+	
+
+}
